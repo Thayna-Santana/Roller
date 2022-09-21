5eb7a0716657de24841ad11f6dd2ab1eb88ce79d
Daniel-DaMata <daniel.soares.damata@gmail.com>
2022-09-20 21:14:35 -0300

    Prova-NF

---
+++ app/src/main/java/com/example/prova_teste/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.prova_teste
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice(1..6) }
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        }
+}
